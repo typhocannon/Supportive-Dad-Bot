7573d6c7f1aeb3ed6d976f2a7bf91c80871ceedf
Casey Tran <ctran59@ucsc.edu>
2022-09-12 00:01:17 -0700

    webscraping commented out

--- dadbot.py
+++ dadbot.py
@@ -6,7 +6,7 @@ import random
 import string
 from dotenv import load_dotenv
 import requests
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
 # checking intents to be able to send msgs
 intents = discord.Intents.default()
@@ -109,7 +109,22 @@ async def on_message(message):
             num = 1
             if num == 1:
                 response = random.choice(father_quotes)
+            #else:
+                #URL = "https://icanhazdadjoke.com/"
+                #page = requests.get(URL)
 
+                #soup = BeautifulSoup(page.content, "html.parser")
+
+                #joke = soup.find("p", "subtitle")
+
+                #joke2 = joke.prettify()
+
+                #joke3 = BeautifulSoup(joke2, 'html.parser')
+                #joke3 = joke3.get_text()
+
+                #joke4 = joke3.strip()
+                #greeting = f"Hey {name.capitalize()} "
+                #response = greeting + joke4
         print(response)
         await message.channel.send(response)
     else:
